1d721bd8d06bb8b482a3511a21544e74a3e60e64
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-09-27 21:30:05 +0530

    Changing State From Another Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,12 +12,18 @@ class Contact extends Component {
         })
     };
 
+    onDeleteClick = () => {
+        this.props.deleteClickHandler();
+    }
+
     render() {
         const { name, email, phone } = this.props.contact;
         const { showContactInfo } = this.state;
         return (
             <div className="card card-body mb-3">
-                <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down" /></h4>
+                <h4>{name} 
+                <i onClick={this.onShowClick} className="fas fa-sort-down" style={{ cursor: 'pointer' }} />
+                <i onClick={this.onDeleteClick} className="fas fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red' }} /></h4>
                 { showContactInfo ? (
                     <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
