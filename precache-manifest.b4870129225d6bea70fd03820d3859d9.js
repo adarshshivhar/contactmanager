96b38b401f20fede43c0d40aee5175f2be12dde0
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-09-30 19:33:35 +0530

    Updates

--- precache-manifest.b4870129225d6bea70fd03820d3859d9.js
+++ precache-manifest.b4870129225d6bea70fd03820d3859d9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92512d30a74780e59a48e8511f7f4c90",
+    "revision": "538669d08a13823d2dd24c929e6a32d8",
     "url": "/contactmanager/index.html"
   },
   {
-    "revision": "9e175204d31a7720b0d4",
+    "revision": "8b8351d8e88850a41856",
     "url": "/contactmanager/static/css/2.aee84c9b.chunk.css"
   },
   {
-    "revision": "bb67f7795fbbf371e9eb",
+    "revision": "1ddb9167fd54f1dbd64c",
     "url": "/contactmanager/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "9e175204d31a7720b0d4",
-    "url": "/contactmanager/static/js/2.14f4c81b.chunk.js"
+    "revision": "8b8351d8e88850a41856",
+    "url": "/contactmanager/static/js/2.dd4f2cc7.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/contactmanager/static/js/2.14f4c81b.chunk.js.LICENSE.txt"
+    "url": "/contactmanager/static/js/2.dd4f2cc7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bb67f7795fbbf371e9eb",
-    "url": "/contactmanager/static/js/main.472809c5.chunk.js"
+    "revision": "1ddb9167fd54f1dbd64c",
+    "url": "/contactmanager/static/js/main.4a2710d5.chunk.js"
   },
   {
     "revision": "78370bb576dc7a6cd44e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/contactmanager/precache-manifest.986f4a3068ceb9c437b96485c1613e3c.js"
+  "/contactmanager/precache-manifest.b4870129225d6bea70fd03820d3859d9.js"
 );
 
 self.addEventListener('message', (event) => {
