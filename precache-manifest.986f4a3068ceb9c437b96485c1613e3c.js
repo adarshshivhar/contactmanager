a622611af4bb23c2bf5f095be380462c6026cfe3
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-09-30 19:20:57 +0530

    Updates

---
+++ precache-manifest.986f4a3068ceb9c437b96485c1613e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92512d30a74780e59a48e8511f7f4c90",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "9e175204d31a7720b0d4",
+    "url": "/contactmanager/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "bb67f7795fbbf371e9eb",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9e175204d31a7720b0d4",
+    "url": "/contactmanager/static/js/2.14f4c81b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contactmanager/static/js/2.14f4c81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb67f7795fbbf371e9eb",
+    "url": "/contactmanager/static/js/main.472809c5.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
