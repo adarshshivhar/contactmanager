8ebf4abecd1eb46840f399332ffb9042ce87b743
Adarsh Shivhare <adarshshivhare789@gmail.com>
2020-09-30 19:00:04 +0530

    Update part 2

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -61,6 +61,19 @@ class EditContact extends Component {
           return;
         }
 
+        const updateContact = {
+            name,
+            email,
+            phone
+        }
+        
+        const { id } = this.props.match.params;
+
+        const res = await axios.put(
+          `https://jsonplaceholder.typicode.com/users/${id}`, updateContact
+        );
+
+        dispatch({ type: 'UPDATE_CONTACT', payload: res.data });
         
         //CLEAR STATE 
         this.setState({
